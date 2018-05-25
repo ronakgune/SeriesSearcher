36ba0f97bcdc645bfe98f1c747559c5cff1b3836
ronakgune <ronakgune@gmail.com>
2018-05-25 10:14:02 -0700

    tests

--- series-searcher/src/App.js
+++ series-searcher/src/App.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Intro = (props) => {
+  <p>Our first functional component</p>
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Series Searcher</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
